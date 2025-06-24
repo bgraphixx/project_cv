311158a4d12d8183db7c4fda8d2554b0ad1d81b3
Ebubechukwu Ibeh <ebubechukwu.ibeh@fewchorefinance.com>
2025-06-24 19:23:36 +0100

    Resume template created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,36 +2,40 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="">
-      <div className="mt-5">
+    <div className="mx-12">
+      <div className="mt-12">
         <hr />
         {/* Personal Information */}
-        <div className="py-3">
-          <h2 className="text-xl">Your Name</h2>
-          <p>Title</p>
+        <div className="flex flex-col gap-2 py-3">
+          <h2 className="text-3xl font-semibold">Your Name</h2>
+          <h3 className="text-green-600 font-semibold">Title</h3>
           <p>Address or Location</p>
         </div>
         {/* Skills Information */}
         <div className="py-3">
-          <h3>Skills</h3>
+          <h3 className="text-green-600 font-semibold">Skills</h3>
           <p>Skills list</p>
         </div>
         {/* Education Information */}
         <div className="py-3">
-          <h3>Education</h3>
-          <div>
-            <h5>School Name, Location - Degree</h5>
+          <h3 className="text-green-600 font-semibold">Education</h3>
+          <div className="flex flex-col gap-1  mt-2">
+            <h2 className="text-blue-700 text-xl">
+              School Name, Location - Degree
+            </h2>
             <p>Enrollment Date - Graduation Date</p>
             <p>List of exciting things you did at university</p>
           </div>
         </div>
         {/* Experience Information */}
         <div className="py-5">
-          <h3>Experience</h3>
-          <div>
-            <h5>Company Name, Location - Job Title</h5>
+          <h3 className="text-green-600 font-semibold">Experience</h3>
+          <div className="flex flex-col gap-1 mt-2">
+            <h2 className="text-blue-700 text-xl">
+              Company Name, Location - Job Title
+            </h2>
             <p>Employment Date - Departure Date</p>
-            <ul className="list">
+            <ul className="ml-5 list-disc">
               <li>List of achievements</li>
               <li>List of achievements</li>
               <li>List of achievements</li>
@@ -42,7 +46,7 @@ const HomePage = () => {
 
         {/* Public Links*/}
         <div className="">
-          <h3>Across the Internet</h3>
+          <h3 className="text-green-600 font-semibold">Across the Internet</h3>
           <p>Add your LinkedIn, GitHub profile links</p>
         </div>
         {/* <Building2 /> */}
